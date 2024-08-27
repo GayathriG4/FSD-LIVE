830886fd164b6d7f9984cbcb4260220b74e03627
GayathriG4 <gayathriarumugam284@gmail.com>
2024-08-28 00:44:18 +0530

    First Commit

---
+++ Day 2/script.js
@@ -0,0 +1,403 @@
+// alert("Email is not found");
+
+
+// let names=prompt("Enter your names");
+// console.log(names);
+
+
+// let a= Number(prompt("Enter the values"));
+// console.log(a+a);
+
+// let g= Number(prompt("Enter values"));
+// console.log(g+g);
+
+
+// let names=prompt("Enter the name");
+// console.log(names);
+
+// let age=Number(prompt("Enter age"));
+// console.log(age);
+
+
+// let a=10
+// let b=7
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a/b);
+// console.log(a**b);
+
+
+// let a=true
+// let b=false
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+// console.log(!b);
+
+
+
+// let c=10;
+// let d=20;
+
+// console.log(c==10 && d==10);
+// console.log(c==10 && d==20);
+// console.log(c==20 || d==20);
+// console.log(c==20 || d==10);
+// console.log(!d==20);
+
+
+
+// function display(params) {
+//     let g=4;
+//     console.log(g);
+    
+// }
+// display();
+
+
+// datatypes
+
+// let k=10;
+// console.log(typeof k) ;
+
+
+// let name="gayu"
+// console.log(typeof name);
+
+
+// let h=false
+// console.log(typeof h);
+
+
+// let s=null;
+// console.log(typeof s);
+
+
+// let obj={name:"giri", age:27}
+// console.log(obj.name);
+// console.log(obj.age);
+
+// let object={name:"gayu",age:27}
+// console.log(object.age);
+
+
+
+// let array=[10,20,30,40,50];
+// console.log(array[3]);
+
+
+
+
+
+
+
+// DAY 3
+
+
+
+
+
+
+//Function // normal
+
+
+
+// let l=10;
+// function firstfunction(params) {
+//     console.log("nor func");
+//     console.log("l");
+// }
+// firstfunction()
+
+
+
+// function display(params) {
+//     console.log("Hello world");
+    
+// }
+// display()
+    
+    
+
+//arrow es6 // real time function
+
+// const arrFun= ()=>{
+//     console.log('arrFun');
+// }
+// arrFun();
+
+
+// Anonymous function
+
+
+
+// const res= function(a,b){
+//     console.log(a,b);
+    
+// }
+// res(10,9);
+
+
+// let res=function(a,b){
+//     console.log(a,b);
+    
+// }
+// res(10,10);
+
+
+//hof higher ooder function
+
+// function norfunc(){
+//     console.log(" nor function");
+    
+// }
+// function hof(){
+//     console.log("higher order function");
+//     return function retfun(){
+//         console.log(" return function");
+        
+//     }
+    
+// }
+
+// const res=hof();
+// res();
+
+//call back
+
+// function norfun(){
+//     console.log("normal function");
+    
+// }
+// function hof(callback){
+//     console.log("higher order function");
+//     callback()
+//     return function retfun(){
+//         console.log("return statemnt");
+        
+//     }
+
+    
+// }
+// function display(){
+//     console.log("function");
+    
+// }
+// const res=hof(display)
+// res();
+
+
+
+
+
+//return statement
+
+
+// function retfun(y,z){
+//     console.log("return ststement");
+//     return y+z;
+    
+// }
+// let val=retfun(50,100)
+// console.log(val);
+
+
+// function display(){
+//     return 10;
+// }
+
+// let retval=display()
+// console.log (retval);
+
+
+// if else
+
+// let login = true;
+
+
+// if(login==true){
+//     console.log("login is successful");
+    
+// }else{
+//     console.log("invalid");
+    
+// }
+
+
+// ternary (if else)
+
+
+
+// const res= login = false ? "home" : "register";
+// console.log(login);
+
+
+// Switch ( multiple if else )
+
+
+
+// let ret
+// let msg;
+
+// switch (ret) {
+//     case 1:
+//        msg ='ordered sunscreen'
+//        break;
+//     case 2:
+//         msg='ordered serum'
+//         break;
+
+//     default:
+//         msg='exchange'
+
+// }
+// console.log(msg);
+
+
+
+
+
+
+// let grade = 'B'
+// let result;
+
+// switch (grade) {
+//     case 'A':
+//         result = "Grade is excellent"
+//         break;
+//     case 'B':
+//         result = "Grade is good"
+//         break;
+//     case 'C':
+//         result = "Grade is Average "
+//         break;
+//     case 'D':
+//         result = "Grade is Poor"
+//         break;
+//     default:
+//         text = "NO grades achieved";
+// }
+// console.log(result)
+
+
+
+//Day4
+
+
+
+//Task
+
+// function t0p() {
+//     console.log("t0p fun");
+
+//     function bottom() {
+//         console.log("bot fun");
+//     }
+
+//     return bottom;
+// }
+
+
+// const bottomFunction = t0p();
+
+// bottomFunction();
+
+
+
+//array
+
+
+
+//  let array=[100,200,300,400,500];
+//  console.log(array.length);
+ 
+
+// //  // push pop
+
+
+// array.push(600);
+// console.log(array);
+
+
+// array.pop();
+// console.log(array);
+
+// // // unshift shift
+
+
+
+
+// array.unshift(1000)
+// console.log(array);
+
+
+// array.shift()
+// console.log(array);
+
+
+
+// slice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
